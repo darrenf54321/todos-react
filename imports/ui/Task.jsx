629e8c2c3a44339b70772ad83c65bbe1158eaac7
Darrenf <darrenf54321@gmail.com>
2017-01-04 16:33:02 +0000

    1. Users can check a box to only see incomplete tasks 2. Checkbox added to App component 3. renderTasks updated to filter out completed tasks 4. show count of incomplete taks added

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -5,19 +5,19 @@ import { Tasks } from '../api/tasks.js';
 // Task component - represents a single todo item
 export default class Task extends Component {
   toggleChecked() {
-  // Set the checked property to the opposite of its current value
-  Tasks.update(this.props.task._id, {
-    $set: { checked: !this.props.task.checked },
-  });
-}
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
 
-deleteThisTask() {
-  Tasks.remove(this.props.task._id);
-}
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
   render() {
     // Give tasks a different className when they are checked off,
-// so that we can style them nicely in CSS
-const taskClassName = this.props.task.checked ? 'checked' : '';
+    // so that we can style them nicely in CSS
+    const taskClassName = this.props.task.checked ? 'checked' : '';
 
     return (
       <li className={taskClassName}>
